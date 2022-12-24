8cc519f6d18b2f22506055e556e8e5ee48caba1a
Idrees Hassan <idrees@idreesinc.com>
2022-12-24 14:30:40 -0500

    Add descents and characters 33 to 126

--- src/monocraft.py
+++ src/monocraft.py
@@ -14,20 +14,21 @@ monocraft.em = PIXEL_SIZE * 9
 monocraft.upos = -PIXEL_SIZE # Underline position
 
 characters = json.load(open("./characters.json"))
-print(characters[0]["codepoint"])
+print(len(characters))
 
 for character in characters:
 	monocraft.createChar(character["codepoint"], character["name"])
 	pen = monocraft[character["name"]].glyphPen()
+	descent = -PIXEL_SIZE * character["descent"] if "descent" in character else 0
 	for rowIndex in range(len(character["pixels"])):
 		row = character["pixels"][-(rowIndex + 1)]
 		for columnIndex in range(len(row)):
 			if row[columnIndex] == 0:
 				continue
-			pen.moveTo((columnIndex * PIXEL_SIZE, rowIndex * PIXEL_SIZE)) # Bottom left
-			pen.lineTo((columnIndex * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE))
-			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE))
-			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, rowIndex * PIXEL_SIZE))
+			pen.moveTo((columnIndex * PIXEL_SIZE, rowIndex * PIXEL_SIZE + descent)) # Bottom left
+			pen.lineTo((columnIndex * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + descent))
+			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + descent))
+			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, rowIndex * PIXEL_SIZE + descent))
 			pen.closePath()
 	monocraft[character["name"]].width = PIXEL_SIZE * 6
 
