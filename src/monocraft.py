1645f30b63bf7f9410b6627041a745339042b828
Idrees Hassan <idrees@idreesinc.com>
2022-12-24 17:17:17 -0500

    Add support for diacritics and reference glyphs

--- src/monocraft.py
+++ src/monocraft.py
@@ -14,22 +14,43 @@ monocraft.em = PIXEL_SIZE * 9
 monocraft.upos = -PIXEL_SIZE # Underline position
 
 characters = json.load(open("./characters.json"))
+diacritics = json.load(open("./diacritics.json"))
+charactersByCodepoint = {}
 print(len(characters))
 
-for character in characters:
-	monocraft.createChar(character["codepoint"], character["name"])
-	pen = monocraft[character["name"]].glyphPen()
-	descent = -PIXEL_SIZE * character["descent"] if "descent" in character else 0
-	for rowIndex in range(len(character["pixels"])):
-		row = character["pixels"][-(rowIndex + 1)]
+def drawGlyph(pixels, pen, startingY):
+	top = 0 # The highest point of the character
+	for rowIndex in range(len(pixels)):
+		row = pixels[-(rowIndex + 1)]
 		for columnIndex in range(len(row)):
 			if row[columnIndex] == 0:
 				continue
-			pen.moveTo((columnIndex * PIXEL_SIZE, rowIndex * PIXEL_SIZE + descent)) # Bottom left
-			pen.lineTo((columnIndex * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + descent))
-			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + descent))
-			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, rowIndex * PIXEL_SIZE + descent))
+			pen.moveTo((columnIndex * PIXEL_SIZE, rowIndex * PIXEL_SIZE + startingY)) # Bottom left
+			pen.lineTo((columnIndex * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + startingY))
+			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, (rowIndex + 1) * PIXEL_SIZE + startingY))
+			pen.lineTo(((columnIndex + 1) * PIXEL_SIZE, rowIndex * PIXEL_SIZE + startingY))
 			pen.closePath()
+			top = (rowIndex + 1) * PIXEL_SIZE + startingY
+	return top
+
+def drawCharacter(character, pen):
+	floor = -PIXEL_SIZE * character["descent"] if "descent" in character else 0
+	return drawGlyph(character["pixels"], pen, floor)
+
+for character in characters:
+	charactersByCodepoint[character["codepoint"]] = character
+	monocraft.createChar(character["codepoint"], character["name"])
+	pen = monocraft[character["name"]].glyphPen()
+	if "pixels" in character:
+		drawCharacter(character, pen)
+	elif "reference" in character:
+		top = drawCharacter(charactersByCodepoint[character["reference"]], pen)
+		if "diacritic" in character:
+			diacritic = diacritics[character["diacritic"]]
+			if "diacriticSpace" in character:
+				top += PIXEL_SIZE * character["diacriticSpace"]
+			drawGlyph(diacritic["pixels"], pen, top)
+
 	monocraft[character["name"]].width = PIXEL_SIZE * 6
 
 monocraft.generate("generated.ttf")
