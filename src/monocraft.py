925016fc05734b2cbac4cddc6ddf939efde0113f
Nekosha <79341606+Nekosha@users.noreply.github.com>
2024-02-18 21:55:43 +0700

    generate: allow to specify where to place diacritics
    Various scripts have diacritics on positions other than the top of base
    characters. This commit adds support for the (optional) `placement` key
    in diacritic.json, allowing diacritics to be placed in any of the four
    positions: above, below, to the left, and to the right of the glyph.
    The default when not specified is "above".
    The placement respects the spacing specified by the `diacriticSpace`
    key in characters.json.

--- src/monocraft.py
+++ src/monocraft.py
@@ -265,21 +265,64 @@ def generateImage(character):
 		kw.update(other[1])
 		image = image | other[0]
 	if "diacritic" in character:
-		diacritic = diacritics[character["diacritic"]]
-		arr = diacritic["pixels"]
-		x = image.x
-		y = findHighestY(image) + 1
-		if "diacriticSpace" in character:
-			y += int(character["diacriticSpace"])
-		image = image | imageFromArray(arr, x, y)
+		image = addDiacritic(image, diacritics[character["diacritic"]], character["diacriticSpace"])
 	return (image, kw)
 
+def addDiacritic(image, diacritic, spacing):
+	arr = diacritic["pixels"]
+	direction = [0, 1]
+	if "placement" in diacritic:
+		if "above" in diacritic["placement"]:
+			direction[1] = 1
+		elif "below" in diacritic["placement"]:
+			direction[1] = -1
+		if "left" in diacritic["placement"]:
+			direction[0] = -1
+		elif "right" in diacritic["placement"]:
+			direction[0] = 1
+	x, y = findBoundsInDirection(image, direction)
+	x += direction[0]
+	y += direction[1]
+	if spacing is not None:
+		x += int(spacing) * direction[0]
+		y += int(spacing) * direction[1]
+	return image | imageFromArray(arr, x, y)
+
+def findBoundsInDirection(image, direction):
+	if   direction[0] == 0: x = image.x
+	elif direction[0] >  0: x = findHighestX(image)
+	elif direction[0] <  0: x = findLowestX(image)
+
+	if   direction[1] == 0: y = image.y
+	elif direction[1] >  0: y = findHighestY(image)
+	elif direction[1] <  0: y = findLowestY(image)
+
+	return x, y
+
+def findHighestX(image):
+	for x in range(image.x_end - 1, image.x, -1):
+		for y in range(image.y, image.y_end):
+			if image[x, y]:
+				return x
+	return image.x
+def findLowestX(image):
+	for x in range(image.x, image.x_end):
+		for y in range(image.y, image.y_end):
+			if image[x, y]:
+				return x
+	return image.x_end
 def findHighestY(image):
 	for y in range(image.y_end - 1, image.y, -1):
 		for x in range(image.x, image.x_end):
 			if image[x, y]:
 				return y
 	return image.y
+def findLowestY(image):
+	for y in range(image.y, image.y_end):
+		for x in range(image.x, image.x_end):
+			if image[x, y]:
+				return y
+	return image.y_end
 
 def imageFromArray(arr, x=0, y=0):
 	return PixelImage(
